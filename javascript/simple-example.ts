7a28f251b28a5dc8e9f1a23faeb9443b01b5e21e
MyungSub0519 <shinms0519@gmail.com>
2025-11-04 21:41:10 +0900

    ADD: typescript portal-tunnel example

---
+++ javascript/simple-example.ts
@@ -0,0 +1,19 @@
+import http from "http";
+import { portalTunnel } from "./utils/portal-tunnel";
+
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello, World!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+
+  portalTunnel({
+    port: PORT,
+    name: "ts-test-tunnel",
+    relay: "wss://portal.gosuda.org/relay",
+    // logLevel: "silent",
+  });
+});
